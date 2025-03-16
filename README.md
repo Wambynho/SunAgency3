fb9001d5fa9c30cfc2939d4f741cbb54456986c9
TonNom <ton-email@gmail.com>
2025-03-16 16:08:46 +0100

    Premier commit du projet Sun Agency

---
+++ README.md
@@ -0,0 +1,2 @@
+# Framer Website
+Ce projet est conçu pour être déployé sur Vercel avec React.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue sur Sun Agency</h1>
+      <p>Votre chasseur immobilier pour des biens ensoleillés à l'international.</p>
+    </div>
+  );
+}
+
+export default App;
